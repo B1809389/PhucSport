eeb1b5afe71ca623f2d691e3e1cca25d1fc0b46a
b1809128 <nghoquochuy0902@gmail.com>
2021-11-19 00:08:55 +0700

    update all

--- getImage.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $sql_image = "select * from hinhhanghoa";
-    $result_image = mysqli_query($con, $sql_image);
-    while ($row = mysqli_fetch_array($result_image)){
-        $row['TenHinh'];
-    }
